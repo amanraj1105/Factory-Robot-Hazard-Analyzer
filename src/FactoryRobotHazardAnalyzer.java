2f4ac8c5d722c078aeb5be5d81624c4ef1620f9c
amanraj1105 <aman1105raj@gmail.com>
2026-01-29 14:35:59 +0530

    [Aman]: UC1: [UC1] Display static hazard analyzer message

---
+++ src/FactoryRobotHazardAnalyzer.java
@@ -0,0 +1,11 @@
+/**
+ * FactoryRobotHazardAnalyzer - UC1
+ *
+ * Displays a static message indicating the purpose of the system.
+ */
+public class FactoryRobotHazardAnalyzer {
+
+    public static void main(String[] args) {
+        System.out.println("Factory Robot Hazard Analyzer");
+    }
+}
